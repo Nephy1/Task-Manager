7259a1de20c028e99813e9578d98811919786ebd
Devon Lipsius <devon_3273@hotmail.com>
2022-05-14 21:31:42 -0600

    added controller/commands

---
+++ Command/CreateTaskCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using task_app.ViewModel;
+
+namespace task_app.Command
+{
+    public class CreateTaskCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            if (parameter is TaskListViewModel taskList)
+            {
+                taskList.Tasks.Add(new TaskViewModel() { Name = taskList.TaskName, Complete = false });
+            }
+        }
+    }
+}
