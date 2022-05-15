ca82e8f87c965c3f99f5412149fdb24ef58baa63
Devon Lipsius <devon_3273@hotmail.com>
2022-05-14 21:03:59 -0600

    added models, changed typo in csproj file

---
+++ ViewModel/TaskListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_app.ViewModel
+{
+    public class TaskListViewModel
+    {
+        public List<TaskViewModel> Tasks { get; set; }
+        public string TaskName { get; set; }
+    }
+}
